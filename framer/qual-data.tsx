810bd8caec8d759eb5203d12aac99580b869f6d7
GillianRickerd <gillianrickerd@gmail.com>
2023-10-13 15:18:37 -0400

    Add qualitative data component for framer

---
+++ framer/qual-data.tsx
@@ -0,0 +1,67 @@
+export default function Qual_data() {
+  return (
+    <div className="frontrow-qual-data" id="frontrow-qual-data">
+    <div className="fr-header">
+      <h1 className="fr-title">
+        What doctors told their patients about our product*
+      </h1>
+      <p>
+        <span className="fr-pr-bold">*188</span> medical providers recently shared our product with patients. This data was collected by a 3rd party. Providers are never paid (in any way) to share our product. These providers remain anonymous because they are not publicly marketing our product. These were notes they wrote to their own patients, and do not constitute medical advice.
+      </p>
+    </div>
+
+    <div className="fr-review-container">
+      <div className="fr-tag-row">
+        <div className="fr-tags">
+          <div className="fr-tag"><i className="fa fa-check-circle"></i> Verified doctor</div>
+          <div className="fr-tag">Not paid to share product</div>
+        </div>
+
+        <div className="doctor-details">Austin, TX | September 2023</div>
+      </div>
+
+      <div className="fr-review-title">
+        ...it’s a critical resource designed for teens to help them manage their health before onset of more serious chronic conditions.
+      </div>
+
+      <div className="fr-review-text">
+        I know a lot of you now have kids that are teenagers, so I’m sharing this because it’s a critical resource designed for teens to help them manage their health before onset of more serious chronic conditions. For example, Thread Health understands that as a teen, your child may have questions or concerns about their health that they might not feel comfortable discussing face-to-face with a doctor. Here, they can text a trained adolescent specialist and talk about sensitive topics like sexual or mental health — things that aren’t discussed enough at an early age with people who are clinically trained to address these issues.
+      </div>
+
+      <div className="doctor-details-mobile">Austin, TX | September 2023</div>
+    </div>
+
+    <div className="fr-review-container">
+      <div className="fr-tag-row">
+        <div className="fr-tags">
+          <div className="fr-tag"><i className="fa fa-check-circle"></i> Verified doctor</div>
+          <div className="fr-tag">Not paid to share product</div>
+        </div>
+
+        <div className="doctor-details">Tallahassee, FL | September 2023</div>
+      </div>
+
+      <div className="fr-review-title">
+        It's like having a healthcare lifeline for your teenager, available at any time...
+      </div>
+
+      <div className="fr-review-text">
+        As a conventional medical doctor, I can tell you first hand that we do not receive enough training on how to manage the health of teens — I’ve had two teenagers myself, and they simply face different challenges that need to be handled more sensitively. What’s nice about Thread and why I’m excited to share it with you is that imagine your teenager wakes up feeling unwell or has questions about their mental health. With Thread Health, they can reach out discreetly to a trained adolescent specialist, without the need for an in-person visit. It's like having a healthcare lifeline for your teenager, available at any time — a reassuring safety net for both you and your child.
+      </div>
+
+      <div className="doctor-details-mobile">Tallahassee, FL | September 2023</div>
+    </div>
+
+    <div className="quant-data-mobile">
+      <p>
+        <span className="fr-pr-bold">*188</span> medical providers recently shared our product with patients.
+      </p>
+      <p>
+        This data was collected by a 3rd party. Providers are never paid (in any way) to share our product. These providers remain anonymous because they are not publicly marketing our product. These were notes they wrote to their own patients, and do not constitute medical advice.
+      </p>
+    </div>
+    <div className="fr-learn-more">
+      <p>Learn more about <a className="fr-learn-more" href="https://thefrontrowhealth.com/overview/" target="_blank">Frontrow Health</a> and the full list of doctors they work with</p>
+    </div>
+  </div>
+)}
