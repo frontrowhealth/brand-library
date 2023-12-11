9d8bb8dbdd1e0214ce673a7b2e4ad6ec1a8c1ba9
GillianRickerd <gillianrickerd@gmail.com>
2023-12-11 14:17:19 -0500

    Branding update (#7)
    * Update template for quant and qual widgets with frontrow branding
    
    * Update widget to new branding including new modal

---
+++ html/insert-into-page.js
@@ -0,0 +1,29 @@
+// Sample reference to use for future API work
+// TODO: consider making this repo private
+
+// JavaScript snippet to fetch HTML content from a URL and insert it into an element
+
+// The URL from which to fetch HTML content
+var url = '';
+
+// Select the element where you want to insert the HTML
+// Snippet will be inserted ABOVE this element
+var element = document.getElementById('');
+
+// Use fetch API to get HTML content from the URL
+fetch(url)
+  .then(response => {
+    // Check if the request was successful
+    console.log(response);
+    if (response.ok) {
+      return response.text(); // Get the response text (HTML content)
+    }
+    throw new Error('Network response was not ok.');
+  })
+  .then(htmlContent => {
+    // Insert the fetched HTML into the element
+    element.insertAdjacentHTML('beforebegin', htmlContent);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation: ', error);
+  });
